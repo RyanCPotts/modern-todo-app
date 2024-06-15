b756df4925ba024ea1550b73af449a90d0a2c111
RYAN <ryancpotts@gmail.com>
2024-06-15 16:17:37 -0500

    lab 32 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import SettingsForm from './Context/SettingsForm';
+
 import { MantineProvider } from '@mantine/core';
 
-import SettingsProvider from './Context/Settings';
+import SettingsProvider from './Context/Settings'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Todo from './Components/Todo';
 
@@ -10,13 +14,22 @@ import '@mantine/core/styles.css';
 
 import './site.scss';
 
+import Header from './Components/Header';
+
 const App = () =>{
     return (
-      <SettingsProvider>
-        <MantineProvider>
-          <Todo />
-        </MantineProvider>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <SettingsProvider>
+        <BrowserRouter>
+          <Header/>
+          {/* <Todo /> */}
+          <Routes>
+            <Route path = '/' element = {<Todo/>}/>
+            <Route path = '/settings' element = {<SettingsForm/>}/>
+        </Routes>
+      </BrowserRouter>
       </SettingsProvider>
+      </MantineProvider>
     );
   }
 
