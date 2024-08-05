37d5e4fdc0a7314f14e8fd034daec7ac2fd86b8c
RYAN <ryancpotts@gmail.com>
2024-08-04 20:48:33 -0500

    Todo App functionality complete. Need more styling!

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,3 @@
-// import React from 'react';
-
-// import SettingsForm from './Context/SettingsForm';
-
-// import { MantineProvider } from '@mantine/core';
-
-// import SettingsProvider from './Context/Settings';
-
-// import LoginProvider from './Components/Auth/context';
-
-// import Login from './Components/Auth/login';
-
-// import Auth from './Components/Auth/auth'
-
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
-
-// import Todo from './Components/Todo';
-
-// // import '@mantine/core/styles.css';
-
-// import './site.scss';
-
-// import Header from './Components/Header';
-
-// const App = () =>{
-//     return (
-//       <MantineProvider withGlobalStyles withNormalizeCSS>
-//         <LoginProvider>
-//         <SettingsProvider>
-//         <BrowserRouter>
-//           {/* <Header/> */}
-//           <Login/>
-//           {/* <Todo /> */}
-//           <Routes>
-//             <Route path = '/' element = {<Todo/>}/>
-//             <Route path = '/settings' element = {<SettingsForm/>}/>
-//         </Routes>
-//         <Auth><div>any user can see this</div></Auth>
-//       </BrowserRouter>
-//       </SettingsProvider>
-//       </LoginProvider>
-//       </MantineProvider>
-//     );
-//   }
-
-// export default App
-
 import React from 'react';
 import SettingsForm from './Context/SettingsForm';
 import { MantineProvider } from '@mantine/core';
@@ -55,25 +8,24 @@ import Auth from './Components/Auth/auth';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Todo from './Components/Todo';
 import './site.scss';
-import Header from './Components/Header';
+// import Header from './Components/Header';
 
 const App = () => {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-  <LoginProvider>
-    <SettingsProvider>
-      <BrowserRouter>
-        <Login />
-        <Routes>
-          <Route path='/' element={<Todo />} />
-          <Route path='/settings' element={<SettingsForm />} />
-        </Routes>
-        <Auth><div>any user can see this</div></Auth>
-      </BrowserRouter>
-    </SettingsProvider>
-  </LoginProvider>
-</MantineProvider>
-
+      <LoginProvider>
+        <SettingsProvider>
+          <BrowserRouter>
+            <Login />
+            <Routes>
+              <Route path='/' element={<Todo />} />
+              <Route path='/settings' element={<SettingsForm />} />
+            </Routes>
+            <Auth><div>any user can see this</div></Auth>
+          </BrowserRouter>
+        </SettingsProvider>
+      </LoginProvider>
+    </MantineProvider>
   );
 };
 
--- src/Components/Todo/index.jsx
+++ src/Components/Todo/index.jsx
@@ -1,24 +1,22 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { Pagination } from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+import {
+  Container,
+  Title,
+  Pagination,
+  Space,
+  Group,
+  Text
+} from '@mantine/core';
 import useForm from '../../hooks/form';
-
 import { v4 as uuid } from 'uuid';
-
-import SettingsProvider from '../../Context/Settings';
-
 import Header from '../Header';
 import List from '../List';
 import Form from '../Form';
-import Auth from '../Auth/auth'
+import Auth from '../Auth/auth';
 
 const Todo = () => {
-  // const context = useContext(SettingsProvider);
-  const [defaultValues] = useState({difficulty:4})
-
-  // const {pageItems, showCompleted} = useContext(SettingsProvider)
-
+  const [defaultValues] = useState({ difficulty: 4 });
   const [list, setList] = useState([]);
-  const [displayList, setDisplayList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
@@ -26,67 +24,63 @@ const Todo = () => {
   function addItem(item) {
     item.id = uuid();
     item.complete = false;
-    console.log(item);
     setList([...list, item]);
   }
 
   function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
+    const items = list.filter(item => item.id !== id);
     setList(items);
   }
 
   function toggleComplete(id) {
-
-    const items = list.map( item => {
-      if ( item.id === id ) {
-        item.complete = ! item.complete;
+    const items = list.map(item => {
+      if (item.id === id) {
+        item.complete = !item.complete;
       }
       return item;
     });
-
     setList(items);
-
   }
 
-
-  
-
   useEffect(() => {
-
-    let filteredList = list.filter( item =>
-    //    {
-    //   return defaultValues.showCompleted ? true : item.complete === false;
-    // }
-    !item.complete).length;
-  // };
-
-    // let start = pageItems * (currentPage - 1);
-    // let end = start + pageItems;
-
-    // setDisplayList(showCompleted? filteredList.slice(start, end): []);
-    // console.log(displayList)
-
-    let incompleteCount = filteredList;
+    let incompleteCount = list.filter(item => !item.complete).length;
     setIncomplete(incompleteCount);
-
     document.title = `To Do List: ${incompleteCount}`;
-
-
   }, [list]);
-
-  // let numPages = Math.ceil(incomplete / pageItems);
 
   return (
     <>
-
       <Header />
-      <Auth capability='create'>
-      <Form handleChange={handleChange} handleSubmit={handleSubmit} difficulty={defaultValues.difficulty} />
-      </Auth>
-      <Auth capability = {'read'}>
-      <List list={list} toggleComplete={toggleComplete} deleteItem={deleteItem} setCurrentPage ={setCurrentPage} currentPage = {currentPage}/>
-      </Auth>
-
+      <Container size="sm" style={{ marginTop: '2rem' }}>
+        <Title order={1} align="center" style={{ marginBottom: '1rem' }}>
+        
+        </Title>
+        <Auth capability="create">
+          <Form
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            difficulty={defaultValues.difficulty}
+          />
+        </Auth>
+        <Space h="md" />
+        <Auth capability="read">
+          <Group position="apart">
+            <Text>Incomplete Tasks: {incomplete}</Text>
+            <Pagination
+              total={Math.ceil(incomplete / 10)}
+              page={currentPage}
+              onChange={setCurrentPage}
+            />
+          </Group>
+          <List
+            list={list}
+            toggleComplete={toggleComplete}
+            deleteItem={deleteItem}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          />
+        </Auth>
+      </Container>
     </>
   );
 };
